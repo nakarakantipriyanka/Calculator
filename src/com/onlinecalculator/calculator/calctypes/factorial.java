658d6dd674f0705bb1149c52acbe7fb69c785ec2
iStar <iStar@192.168.100.14>
2017-05-25 10:43:48 +0530

    factorial program code

---
+++ src/com/onlinecalculator/calculator/calctypes/factorial.java
@@ -0,0 +1,16 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+public class factorial { 
+	public static void main(String args[]){
+		int i, n, fact=1;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the num");
+		n=sc.nextInt();
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		System.out.println("factorial of given number:"+fact);
+	}
+			
+
+}
